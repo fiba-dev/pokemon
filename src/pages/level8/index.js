432d530dbce2a9561072f541b0c2d8479a918bed
fiba-dev <fibarrola06@gmail.com>
2023-05-26 20:16:43 -0400

    Second update

--- src/pages/level8/index.js
+++ src/pages/level8/index.js
@@ -5,31 +5,44 @@ import { obtenerListaLvl9 } from "../../api";
 import { addPokemonToList } from "../../actions/pokemonActions";
 import "./index.css";
 import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { Loading } from "../../components/ui/loading";
 
 function Level8() {
 	let { winner } = useSelector((state) => state.showCardsPokemons);
 	let dispatch = useDispatch();
 	let navigate = useNavigate();
+	const [loader, setLoader] = useState(false);
 	async function MostrarPokemones() {
-		let pokemones = await obtenerListaLvl9();
+		setLoader(true);
+		try {
+			let pokemones = await obtenerListaLvl9();
 
-		dispatch(addPokemonToList(pokemones));
-		navigate("/level9");
+			dispatch(addPokemonToList(pokemones));
+			navigate("/level9");
+		} catch (error) {
+			// Manejar el error si la llamada falla
+		} finally {
+			setLoader(false);
+		}
 	}
 	return (
 		<div className="level8Container">
-			{" "}
-			<div className="cardsContainer">
-				<AllCards></AllCards>{" "}
-				<ButtonContinue
-					mostrar={winner}
-					onClick={MostrarPokemones}
-					message="Ganaste!"
-					cant={30}
-				>
-					Mas Pokemones
-				</ButtonContinue>
-			</div>
+			{loader ? (
+				<Loading></Loading>
+			) : (
+				<div className="cardsContainer">
+					<AllCards></AllCards>{" "}
+					<ButtonContinue
+						mostrar={winner}
+						onClick={MostrarPokemones}
+						message="Ganaste!"
+						cant={30}
+					>
+						Mas Pokemones
+					</ButtonContinue>
+				</div>
+			)}
 		</div>
 	);
 }
